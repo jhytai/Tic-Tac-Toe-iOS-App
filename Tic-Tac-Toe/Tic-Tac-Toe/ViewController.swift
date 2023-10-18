8db3369e51626fad44f24ff9efa9eb063f2dbcf8
jhytai <josue.tai@csu.fullerton.edu>
2023-10-18 12:31:21 -0700

    Created Xcode template files for Tic-Tac-Toe app

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe
+//
+//  Created by Josh Tai on 10/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
